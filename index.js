fc4b029f1bc34bd57594805302e5bf2a95cb38c6
Deryn Brown <Deryn.2017@gmail.com>
2022-09-06 11:00:36 -0500

    commit tree 9482281f39e8f9475f6021ba02d66ad1b535459f

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution here
+// function findMatching(source, sought) {
+//     return source.filter(
+//       (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+//     );
+//   };
+
+//   function fuzzyMatch(source, testString) {
+//     return source.filter(
+//         (possibleMatch) => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     );
+//   }
+
+//   function matchName(source, soughtName) {
+//     return source.filter((record) => record.name === soughtName);
+//   }
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// const drivers = [
+//   {
+//     name: 'Bobby',
+//     hometown: 'Pittsburgh' },
+//   {
+//     name: 'Sammy',
+//     hometown: 'New York' } ,
+//   {
+//     name: 'Sally',
+//     hometown: 'Cleveland' },
+//   {
+//     name: 'Annette',
+//     hometown: 'Los Angeles' },
+//   {
+//     name: 'Bobby',
+//     hometown: 'Tampa Bay' }
+// ];
+
+function findMatching(driversList, stringMatch) {
+  return driversList.filter(
+    (matchingName) => matchingName.toLowerCase() === stringMatch.toLowerCase()
+  );
+  debugger;
+};
+findMatching(drivers, 'Bobby');
+
+function fuzzyMatch(driversList, stringMatch) {
+  return driversList.filter(
+    (matchingName) => matchingName.toLowerCase().indexOf(stringMatch.toLowerCase()) === 0
+    );
+}
+fuzzyMatch(drivers, 'S');
+
+function matchName(driversList, stringMatch) {
+  return driversList.filter((nameString) => nameString.name === stringMatch);
+}
+debugger;
+matchName(drivers, 'Bobby');
